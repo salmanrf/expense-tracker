966a4f7236f90faeeb0135b8f70efff40a0bb978
salmanrf <salmanrf2@gmail.com>
2023-10-23 17:20:46 +0700

    feat: create mutation with auto create cat, mutation report formatted date

--- src/mutations/mutations.module.ts
+++ src/mutations/mutations.module.ts
@@ -4,9 +4,14 @@ import { MutationsController } from './mutations.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MutationEntity } from './entities/mutations.entity';
 import { UsersModule } from 'src/users/users.module';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MutationEntity]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([MutationEntity]),
+    CategoriesModule,
+    UsersModule,
+  ],
   controllers: [MutationsController],
   providers: [MutationsService],
   exports: [MutationsService],
